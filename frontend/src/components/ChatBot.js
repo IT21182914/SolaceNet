9b7826071661887c98cfac405c9659809e0f9a78
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-30 02:24:12 +0530

    Bug Fix

--- frontend/src/components/ChatBot.js
+++ frontend/src/components/ChatBot.js
@@ -56,23 +56,23 @@ const ChatBot = () => {
       "hello": "Hello! How can I assist you today?",
       "hi": "Hi there! Feel free to ask me anything about mental health or general health.",
       "how are you": "I'm just a computer program, but I'm here to help! How can I assist you today?",
-      "what is mental health": "Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act.",
-      "how to reduce stress": "Try deep breathing exercises or take a short walk to reduce stress.",
-      "what is anxiety": "Anxiety is a normal human emotion that responds to stress, but excessive anxiety can be a mental health disorder.",
-      "how to improve mental health": "Maintain a balanced diet, exercise regularly, get enough sleep, and seek support from friends or professionals.",
-      "tell me about depression": "Depression is a mood disorder that affects how you feel, think, and handle daily activities.",
-      "tell me about mindfulness": "Mindfulness is the practice of bringing one's attention to the present moment.",
-      "how to handle insomnia": "Establish a regular sleep routine, avoid caffeine and electronics before bedtime, and create a comfortable sleep environment.",
+      "mental health": "Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act.",
+      "reduce stress": "Try deep breathing exercises or take a short walk to reduce stress.",
+      "anxiety": "Anxiety is a normal human emotion that responds to stress, but excessive anxiety can be a mental health disorder.",
+      "improve mental health": "Maintain a balanced diet, exercise regularly, get enough sleep, and seek support from friends or professionals.",
+      "depression": "Depression is a mood disorder that affects how you feel, think, and handle daily activities.",
+      "mindfulness": "Mindfulness is the practice of bringing one's attention to the present moment.",
+      "handle insomnia": "Establish a regular sleep routine, avoid caffeine and electronics before bedtime, and create a comfortable sleep environment.",
       "benefits of exercise": "Exercise can improve mood, reduce stress, and contribute to overall physical and mental well-being.",
-      "what is PTSD": "Post-traumatic stress disorder (PTSD) is a mental health condition that can develop after experiencing or witnessing a traumatic event.",
-      "how to deal with loneliness": "Connect with friends and family, engage in social activities, and consider talking to a mental health professional for support.",
+      "PTSD": "Post-traumatic stress disorder (PTSD) is a mental health condition that can develop after experiencing or witnessing a traumatic event.",
+      "loneliness": "Connect with friends and family, engage in social activities, and consider talking to a mental health professional for support.",
       // Add more questions and answers...
 
       // Default response guidance
       "default": "I'm sorry, I don't have information on that specific topic. You can ask me about mental health or general health!"
     };
 
-    // Check if the question has a predefined answer
+    // Check if the question contains keywords and has a predefined answer
     for (const key in questionAnswers) {
       if (question.includes(key)) {
         return questionAnswers[key];
