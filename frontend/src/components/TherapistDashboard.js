46acc83fb91d7d0c4638ad08ae8fcd1dbc3c72a0
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-30 08:26:49 +0530

    Updates

--- frontend/src/components/TherapistDashboard.js
+++ frontend/src/components/TherapistDashboard.js
@@ -1,23 +1,9 @@
-// components/TherapistDashboard.js
-import React, { useState } from 'react';
-import ChatRequest from './ChatRequest';
+import React from 'react'
 
-const TherapistDashboard = () => {
-  const [chatRequests, setChatRequests] = useState([]);
-
-  const handleAcceptRequest = (requestId) => {
-    // Implement the logic to handle the acceptance of the chat request
-    // You can update the state or make an API call to notify the backend
-  };
-
+function TherapistDashboard() {
   return (
-    <div>
-      <h2>Therapist Dashboard</h2>
-      {chatRequests.map((request) => (
-        <ChatRequest key={request.requestId} request={request} onAccept={handleAcceptRequest} />
-      ))}
-    </div>
-  );
-};
+    <div>TherapistDashboard</div>
+  )
+}
 
-export default TherapistDashboard;
+export default TherapistDashboard
