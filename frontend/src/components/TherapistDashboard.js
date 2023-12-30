6ebd9c854e6471db69e58c200d1c92ac77c2359e
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-12-31 00:20:46 +0530

    Modify

--- frontend/src/components/TherapistDashboard.js
+++ frontend/src/components/TherapistDashboard.js
@@ -1,34 +1,41 @@
-// TherapistDashboard.js
+// frontend/src/components/TherapistDashboard.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import MessageCard from './MessageCard'; // Import MessageCard component
 
 const TherapistDashboard = () => {
   const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
         const response = await axios.get('http://localhost:8000/api/messages');
         setMessages(response.data.messages);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching messages:', error);
+        setLoading(false);
       }
     };
 
-    // Call the fetchMessages function when the component mounts
     fetchMessages();
-  }, []); // Empty dependency array to ensure the effect runs once
+  }, []);
 
   return (
     <div>
       <h2>Therapist Dashboard</h2>
-      <ul>
-        {messages.map((message) => (
-          <li key={message._id}>
-            <strong>{message.sender}</strong>: {message.content}
-          </li>
-        ))}
-      </ul>
+      {loading ? (
+        <p>Fetching messages...</p>
+      ) : (
+        <ul>
+          {messages.map((message) => (
+            <li key={message._id}>
+              <MessageCard message={message} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
